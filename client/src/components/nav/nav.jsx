48306075ef54183c0cbcdbd8c4ba61450d3eabac
Gustavo Lara <126248749+gustavo-lara@users.noreply.github.com>
2024-12-18 20:28:03 +0000

    responsividade

--- client/src/components/nav/nav.jsx
+++ client/src/components/nav/nav.jsx
@@ -1,19 +1,17 @@
 import styles from "./nav.module.css";
 import logo from "../../assets/logo.svg";
-import cart from "../../assets/cart.svg";
 import CartButton from "../cartButton/cartButton";
 
 const Nav = () => {
   return (
     <div className={styles.bar}>
-        <div className={styles.logo}>
-            <img src={logo} alt="Logo da DevSteam" width={"72"}/>
-            <h1>DevSteam</h1>
-        </div>
-        <input type="text" placeholder="Buscar"/>
-        <CartButton> </CartButton>
+      <div className={styles.logo}>
+        <img src={logo} alt="Logo da DevSteam" width={"72"} />
+        <h1>DevSteam</h1>
+      </div>
+      <CartButton />
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
